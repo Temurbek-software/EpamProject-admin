e6e1a757d355332e27e6cd8dac2b3e152ff8868e
Temurbek-software <temurbek.zaripov140996@mail.ru>
2022-06-18 01:24:02 +0500

    For ProductController

--- src/main/java/controller/ProductController.java
+++ src/main/java/controller/ProductController.java
@@ -1,4 +1,53 @@
 package controller;
 
-public class ProductController {
+import entity.Category;
+import entity.Product;
+import services.ProductServices;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebServlet("/product")
+public class ProductController extends HttpServlet {
+
+    private ProductServices productService;
+
+    public void init()
+    {
+        productService = new ProductServices();
+    }
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getServletPath();
+
+        try {
+            switch (action) {
+//                case "/categoryNews":
+//                    listOfCategory(request, response);
+//                    break;
+                default:
+                    getAllProducts(request, response);
+                    break;
+            }
+        } catch (SQLException ex) {
+            throw new ServletException(ex);
+        }
+    }
+ private void getAllProducts(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException, ServletException {
+        List<Product> productList = ProductServices.getAllProduct();
+        request.setAttribute("productList", productList);
+        System.out.println("Product list");
+        RequestDispatcher dispatcher = request.getRequestDispatcher("home.jsp");
+        dispatcher.forward(request, response);
+    }
 }
