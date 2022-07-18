4124b3b579b3d2eb0e27ee3375de245a8038c8b0
Temurbek-software <temurbek.zaripov140996@mail.ru>
2022-07-18 11:34:25 +0500

    Created Cookie service

---
+++ src/main/java/services/CookieService.java
@@ -0,0 +1,30 @@
+package services;
+
+import entity.Publisher;
+import entity.Users;
+import payload.PublisherDto;
+import payload.UserDto;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+public class CookieService {
+    public Publisher getPublisher(HttpServletRequest request) {
+        Publisher publisher = new Publisher();
+        PublisherDto publisherDto = new PublisherDto();
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("Username")) {
+                    publisherDto.setUsername(cookie.getValue());
+                }
+                if (cookie.getName().equals("Password")) {
+                    publisherDto.setPassword(cookie.getValue());
+                }
+            }
+        }
+        PublisherService publisherService = new PublisherService();
+        publisher = publisherService.getPublisher(publisherDto);
+        return publisher;
+    }
+}
