212c2187585e34dcfaa67a00e88804b4988a6a9d
Temurbek-software <temurbek.zaripov140996@mail.ru>
2022-07-18 11:31:07 +0500

    Update ProductController.java

--- src/main/java/controller/ProductController.java
+++ src/main/java/controller/ProductController.java
@@ -1,12 +1,12 @@
 package controller;
 
 
+import database.DB;
 import entity.Category;
 import entity.Product;
+import entity.Publisher;
 import payload.ProductDetails;
-import services.CategoryServices;
-import services.ProductServices;
-import services.UserServices;
+import services.*;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -18,22 +18,27 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.SQLException;
+import java.text.ParseException;
 import java.util.List;
 import javax.servlet.http.Part;
 
-@WebServlet(urlPatterns = {"/","/createNews","/showNewsForm"})
+@WebServlet(urlPatterns = {"/", "/deletedNews",
+        "/createNews", "/editNewsForm", "/showNewsForm"
+        ,"/deleteNews","/recover","/deleteNewsTemporary"})
 @MultipartConfig(maxFileSize = 16177215)
-public class ProductController extends HttpServlet
-{
+public class ProductController extends HttpServlet {
     private ProductServices productService;
     private CategoryServices categoryServices;
     private UserServices userServices;
+    private PublisherService publisherService;
+    private CookieService cookieService;
 
-    public void init()
-    {
+    public void init() {
         userServices = new UserServices();
         productService = new ProductServices();
         categoryServices = new CategoryServices();
+        publisherService = new PublisherService();
+        cookieService = new CookieService();
     }
 
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -44,67 +49,143 @@ public class ProductController extends HttpServlet
         String action = request.getServletPath();
         try {
             switch (action) {
-//                case "/edit":
-//                    listOfProducts(request, response);
-//                    break;
                 case "/createNews":
                     insertNews(request, response);
+                    break;
+                case "/displayNews":
+                    displayNews(request, response);
                     break;
                 case "/showNewsForm":
                     newsForm(request, response);
                     break;
-                case "/":
+                case "/editNewsForm":
+                    updateNewsForm(request, response);
+                    break;
+                case "/deletedNews":
+                    deletedPostList(request, response);
+                    break;
+                case "/deleteNews":
+                    deletePost(request, response);
+                    break;
+                case "/deleteNewsTemporary":
+                    deletePostForTime(request, response);
+                    break;
+                case "/recover":
+                    recoverPost(request, response);
+                    break;
+
+                case "/personalMsg":
+                    displayComplain(request, response);
+                    break;
+                default:
                     getStatistic(request, response);
                     break;
             }
-        }
-        catch (SQLException ex) {
+        } catch (SQLException | ParseException ex) {
             throw new ServletException(ex);
         }
     }
 
+
+
+    private void deletePostForTime(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException {
+        long id = Integer.parseInt(request.getParameter("id"));
+        if (productService.deleteTemporaryPost(id))
+        {
+            request.setAttribute("msgDelete","Post recovered successfully");
+        }
+        RequestDispatcher dispatcher = request.getRequestDispatcher("product/ManageNews.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void recoverPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException {
+        long id = Integer.parseInt(request.getParameter("id"));
+        if (productService.recoverPost(id))
+        {
+             request.setAttribute("msgRecover","Post deleted successfully");
+        }
+        RequestDispatcher dispatcher = request.getRequestDispatcher("product/deletetNews.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void deletePost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException {
+        long id = Integer.parseInt(request.getParameter("id"));
+        if (productService.deletePost(id))
+        {
+            request.setAttribute("msg","Post has been deleted successfully");
+        }
+        else
+        {
+            request.setAttribute("msg1","Sorry, Unfortunately post not deleted, please check again");
+        }
+        RequestDispatcher dispatcher = request.getRequestDispatcher("product/deletetNews.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void deletedPostList(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("product/deletetNews.jsp");
+        dispatcher.forward(request, response);
+    }
+    private void displayNews(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException, ParseException
+    {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("product/ManageNews.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void displayComplain(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("publisher/PublisherComplaining.jsp");
+        dispatcher.forward(request, response);
+    }
+
     public void getStatistic(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         int numberOfRegisteredUser = userServices.getCount();
-        request.setAttribute("numOfUsers",numberOfRegisteredUser);
+        request.setAttribute("numOfUsers", numberOfRegisteredUser);
         RequestDispatcher dispatcher = request.getRequestDispatcher("home.jsp");
         dispatcher.forward(request, response);
     }
+
     private void getAllProducts(HttpServletRequest request, HttpServletResponse response)
             throws SQLException, IOException, ServletException {
-//        List<Product> productList = productService.getAllProduct();
-//        request.setAttribute("productList", productList);
-//        System.out.println("Product list");
         List<Category> categoryList = categoryServices.categoryList();
         request.setAttribute("categoryList", categoryList);
         RequestDispatcher dispatcher = request.getRequestDispatcher("product/AddNews.jsp");
         dispatcher.forward(request, response);
     }
+
 
     private void newsForm(HttpServletRequest request, HttpServletResponse response)
-            throws SQLException, IOException, ServletException {
-//        List<Product> productList = productService.getAllProduct();
-//        request.setAttribute("productList", productList);
-//        System.out.println("Product list");
+            throws SQLException, IOException, ServletException, ParseException {
         List<Category> categoryList = categoryServices.categoryList();
         request.setAttribute("categoryList", categoryList);
         RequestDispatcher dispatcher = request.getRequestDispatcher("product/AddNews.jsp");
         dispatcher.forward(request, response);
     }
 
-    private void showFormOfNews(HttpServletRequest request, HttpServletResponse response)
-            throws SQLException, IOException, ServletException
-    {
+    private void updateNewsForm(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException, ServletException, ParseException {
         List<Category> categoryList = categoryServices.categoryList();
         request.setAttribute("categoryList", categoryList);
+        long id = Integer.parseInt(request.getParameter("id"));
+        String name = CategoryServices.getCategoryByName(id);
+        request.setAttribute("nameof", name);
+        Product product = productService.getProductByID(id);
+
+        request.setAttribute("productCurrent", product);
         RequestDispatcher dispatcher = request.getRequestDispatcher("product/AddNews.jsp");
         dispatcher.forward(request, response);
     }
+
     private void insertNews(HttpServletRequest request, HttpServletResponse response)
             throws SQLException, ServletException, IOException {
         String titles = request.getParameter("titles");
         String description = request.getParameter("description");
-        String textData = request.getParameter("textData");
-        System.out.println(textData);
+        String textData = request.getParameter("editor");
         String sourcelinkTo = request.getParameter("sourcelinkTo");
         Part filePart = request.getPart("photofile");
         String categoryName = request.getParameter("name1");
@@ -112,20 +193,17 @@ public class ProductController extends HttpServlet
         product.setTitles(titles);
         product.setDescription(description);
         product.setTextData(textData);
-        System.out.println(product.getTextData());
         product.setSourcelinkTo(sourcelinkTo);
         product.setPhotofile(filePart.getInputStream().readAllBytes());
         product.setCategory_id(Integer.parseInt(categoryName));
-//        System.out.println(product.getCategory_id());
-//        System.out.println(product.getDescription());
-        System.out.println(product.getTextData());
-        int i = productService.saveProduct(product);
-        System.out.println("just watch error");
-        if (i!=0){
-            response.sendRedirect("showNewsForm");
+        CookieService cookieService = new CookieService();
+        long id = cookieService.getPublisher(request).getId();
+        int i = productService.saveProduct(product, id);
+        if (i != 0) {
+            request.setAttribute("msg", "New post saved successfully");
         }
-//        response.sendRedirect("/");
-        // inputP stream of the upload file
+        RequestDispatcher dispatcher = request.getRequestDispatcher("showNewsForm");
+        dispatcher.forward(request, response);
 
     }
 
