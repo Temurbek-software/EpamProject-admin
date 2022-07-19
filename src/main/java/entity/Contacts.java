b7be7b9d692d9d17acec5a698059d468d664fef5
Temurbek-software <temurbek.zaripov140996@mail.ru>
2022-07-19 22:49:14 +0500

    Update Contacts.java

--- src/main/java/entity/Contacts.java
+++ src/main/java/entity/Contacts.java
@@ -13,6 +13,15 @@ public class Contacts {
     public Contacts() {
     }
 
+    public Contacts(long id, String username, String email, String textResponse, Date created_at, Date updated_at) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.textResponse = textResponse;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+    }
+
     public Contacts(String username, String email, String textResponse) {
         this.username = username;
         this.email = email;
