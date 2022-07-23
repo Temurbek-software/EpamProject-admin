92b19fd558529a241059ecedc3b56500c5021872
Temurbek-software <temurbek.zaripov140996@mail.ru>
2022-07-23 15:10:09 +0500

    Update PublisherService.java

--- src/main/java/services/PublisherService.java
+++ src/main/java/services/PublisherService.java
@@ -37,11 +37,11 @@ public class PublisherService {
     private static final String SAVE_PUBLISHER = "INSERT INTO public.publisher(\n" +
             "\t\"nameOfCompany\", address, description, phonenumber," +
             " email, password, username, \"isActive\", \"isBlocked\")\n" +
-            "\tVALUES (?, ?, ?, ?, ?, ?, ?, "+false+", "+false+");";
+            "\tVALUES (?, ?, ?, ?, ?, ?, ?, " + false + ", " + false + ");";
     private static final String VALIDATE_PUBLISHER = "SELECT id, \"nameOfCompany\", address, description, phonenumber, email, password, created_at, updated_at, username\n" +
             "\tFROM public.publisher where username='?' and password='?'";
- private static final String DELETE_PUBLISHER = "DELETE FROM public.publisher\n" +
-         "\tWHERE id=?;";
+    private static final String DELETE_PUBLISHER = "DELETE FROM public.publisher\n" +
+            "\tWHERE id=?;";
 
     public ResultSet getresultSet(String query) throws SQLException {
         Connection connection = DB.getConnection();
@@ -55,21 +55,24 @@ public class PublisherService {
         PreparedStatement preparedStatement = connection.prepareStatement(query);
         return preparedStatement;
     }
+
     public void saveNoisActive(Publisher publisher) {
         try {
-            PreparedStatement preparedStatement = getstatement(
+            Connection connection = DB.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(
                     "UPDATE public.publisher\n" +
-                            "\tSET \"isActive\"='"+false+"'\n" +
-                            "\tWHERE id="+publisher.getId()+";"
+                            "\tSET \"isActive\"='" + false + "'\n" +
+                            "\tWHERE id=" + publisher.getId() + ";"
             );
             int resultSet = preparedStatement.executeUpdate();
+            connection.close();
         } catch (SQLException exception) {
             DB.printSQLException(exception);
         }
     }
 
     public Publisher getPulisher(ResultSet resultSet) throws SQLException, ParseException {
-        DateFormat dateFormat= new SimpleDateFormat("yyyy-mm-dd hh:mm:ss.a");
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss.a");
         Publisher publisher = null;
         Long id = resultSet.getLong("id");
         String username = resultSet.getString("username");
@@ -79,24 +82,26 @@ public class PublisherService {
         String email = resultSet.getString("email");
         String password = resultSet.getString("password");
         String description = resultSet.getString("description");
-        boolean isActive=resultSet.getBoolean("isActive");
-        boolean isBlocked=resultSet.getBoolean("isBlocked");
+        boolean isActive = resultSet.getBoolean("isActive");
+        boolean isBlocked = resultSet.getBoolean("isBlocked");
         Date created_at = dateFormat.parse((dateFormat.format((resultSet.getTimestamp("created_at")))));
         Date updated_at = dateFormat.parse((dateFormat.format((resultSet.getDate("updated_at")))));
         publisher = new Publisher(id, username, nameOfCompany, address, phoneNumber,
-                email, password, description,isActive,isBlocked,
+                email, password, description, isActive, isBlocked,
                 created_at, updated_at);
         return publisher;
     }
 
     public Publisher getPublisherById(long id) {
         Publisher publishersPublisher = new Publisher();
-        try (PreparedStatement preparedStatement = getstatement(GET_PUBLISHER_BY_ID)) {
+        try (
+                Connection connection = DB.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(GET_PUBLISHER_BY_ID)) {
             preparedStatement.setLong(1, id);
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
                 publishersPublisher.setId(resultSet.getLong("id"));
-                publishersPublisher.setNameOfCompany(resultSet.getString("username"));
+                publishersPublisher.setUsername(resultSet.getString("username"));
                 publishersPublisher.setNameOfCompany(resultSet.getString("nameOfCompany"));
                 publishersPublisher.setAddress(resultSet.getString("address"));
                 publishersPublisher.setPhoneNumber(resultSet.getString("phoneNumber"));
@@ -105,6 +110,7 @@ public class PublisherService {
                 publishersPublisher.setPassword(resultSet.getString("password"));
                 publishersPublisher.setBlocked(resultSet.getBoolean("isBlocked"));
             }
+            connection.close();
         } catch (SQLException exception) {
             DB.printSQLException(exception);
         }
@@ -134,60 +140,67 @@ public class PublisherService {
         return resulting;
     }
 
-    public int updatePublisher(Publisher publisher)
-    {
+    public int updatePublisher(Publisher publisher) {
         int resul = 0;
-        try {
-            PreparedStatement preparedStatement = getstatement(
+        try {     Connection connection = DB.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(
                     "UPDATE public.publisher\n" +
-                            "\tSET \"nameOfCompany\"='"+publisher.getNameOfCompany()+"'," +
-                            " address='"+publisher.getAddress()+"',\n" +
-                            "\tdescription='"+publisher.getDescription()+"'," +
-                            " phonenumber='"+publisher.getPhoneNumber()+"',\n" +
-                            "\temail='"+publisher.getEmail()+"'," +
-                            " password='"+publisher.getPassword()+"', " +
-                            "username='"+publisher.getUsername()+"'\n" +
-                            "\tWHERE id="+publisher.getId()+";"
+                            "\tSET \"nameOfCompany\"='" + publisher.getNameOfCompany() + "', " +
+                            "address='" + publisher.getAddress() + "',\n" +
+                            "\tdescription='" + publisher.getDescription() + "'," +
+                            " phonenumber='" + publisher.getPhoneNumber() + "',\n" +
+                            "\temail='" + publisher.getEmail() + "', " +
+                            "password='" + publisher.getPassword() + "', " +
+                            "username='" + publisher.getUsername() + "'\n" +
+                            "\tWHERE id=" + publisher.getId() + ";"
             );
             resul = preparedStatement.executeUpdate();
+            preparedStatement.close();
+            connection.close();
         } catch (SQLException exception) {
             DB.printSQLException(exception);
         }
         return resul;
     }
-    public int  setFalse(long id)
-    {
+
+    public int setFalse(long id) {
         int res = 0;
         try {
-            PreparedStatement preparedStatement = getstatement(
+            Connection connection = DB.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(
                     "UPDATE public.publisher\n" +
-                            "\tSET \"isBlocked\"='"+false+"'\n" +
-                            "\tWHERE id="+id+";"
+                            "\tSET \"isBlocked\"='" + false + "'\n" +
+                            "\tWHERE id=" + id + ";"
             );
-            res  = preparedStatement.executeUpdate();
+            res = preparedStatement.executeUpdate();
+            connection.close();
         } catch (SQLException exception) {
             DB.printSQLException(exception);
         }
         return res;
     }
-    public int setTrue(long idNum)
-    {
-        int resutr=0;
+
+    public int setTrue(long idNum) {
+        int resutr = 0;
         try {
-            PreparedStatement preparedStatement = getstatement(
+            Connection connection = DB.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(
                     "UPDATE public.publisher\n" +
-                            "\tSET \"isBlocked\"='"+true+"'\n" +
-                            "\tWHERE id="+idNum+";"
+                            "\tSET \"isBlocked\"='" + true + "'\n" +
+                            "\tWHERE id=" + idNum + ";"
             );
             resutr = preparedStatement.executeUpdate();
+            connection.close();
         } catch (SQLException exception) {
             DB.printSQLException(exception);
         }
         return resutr;
     }
+
     private String hashPassword(String plainTextPassword) {
         return BCrypt.hashpw(plainTextPassword, BCrypt.gensalt());
     }
+
     public int savePublisher(Publisher publisher) {
         int result = 0;
         System.out.println(SAVE_PUBLISHER);
@@ -196,10 +209,11 @@ public class PublisherService {
              PreparedStatement preparedStatement = connection.prepareStatement(SAVE_PUBLISHER)) {
             preparedStatement.setString(1, publisher.getNameOfCompany());
             preparedStatement.setString(2, publisher.getAddress());
-            preparedStatement.setString(3, publisher.getPhoneNumber());
-            preparedStatement.setString(4, publisher.getEmail());
-            preparedStatement.setString(5,hashPassword(publisher.getPassword()));
-            preparedStatement.setString(6, publisher.getDescription());
+            preparedStatement.setString(3, publisher.getDescription());
+            preparedStatement.setString(4, publisher.getPhoneNumber());
+            preparedStatement.setString(5, publisher.getEmail());
+            preparedStatement.setString(6, publisher.getPassword());
+            preparedStatement.setString(7, publisher.getUsername());
             System.out.println(preparedStatement);
             result = preparedStatement.executeUpdate();
         } catch (SQLException exception) {
@@ -208,52 +222,62 @@ public class PublisherService {
         return result;
     }
 
-    public boolean validate(PublisherDto publisherDto)
-    {
+    public boolean validate(PublisherDto publisherDto) {
         boolean status2 = false;
-        try (PreparedStatement preparedStatement = getstatement("SELECT id, \"nameOfCompany\", address, description, phonenumber, email, password, created_at, updated_at, username\n" +
+        try (
+                Connection connection = DB.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement("SELECT id, \"nameOfCompany\", address, description, phonenumber, email, password, created_at, updated_at, username\n" +
                 "\tFROM public.publisher" +
-                " where username='"+publisherDto.getUsername()+"' and" +
-                " password='"+publisherDto.getPassword()+"'"))
-        {
+                " where username='" + publisherDto.getUsername() + "' and" +
+                " password='" + publisherDto.getPassword() + "'")) {
             ResultSet rs = preparedStatement.executeQuery();
             status2 = rs.next();
+            connection.close();
         } catch (SQLException e) {
             DB.printSQLException(e);
         }
         return status2;
     }
+
     public void saveisActive(Publisher publisher) {
         boolean status = true;
         long idUser = publisher.getId();
         try {
-            PreparedStatement preparedStatement = getstatement(
+            Connection connection = DB.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(
                     "UPDATE public.publisher\n" +
-                            "\tSET \"isActive\"="+status+"\n" +
-                            "\tWHERE id="+idUser+";"
+                            "\tSET \"isActive\"=" + status + "\n" +
+                            "\tWHERE id=" + idUser + ";"
             );
             int resultSet = preparedStatement.executeUpdate();
+            connection.close();
         } catch (SQLException exception) {
             DB.printSQLException(exception);
         }
     }
+
     public boolean deletePublisher(long id) {
         boolean status = false;
-        try (PreparedStatement preparedStatement = getstatement(DELETE_PUBLISHER);) {
+        try (
+                Connection connection = DB.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(DELETE_PUBLISHER);) {
             preparedStatement.setLong(1, id);
             status = preparedStatement.executeUpdate() > 0;
+            connection.close();
         } catch (SQLException exception) {
             exception.printStackTrace();
         }
         return status;
     }
-    public Publisher getPublisher(PublisherDto publisherDto)
-    {
+
+    public Publisher getPublisher(PublisherDto publisherDto) {
         Publisher publisher = new Publisher();
-        try (PreparedStatement preparedStatement = getstatement("SELECT id, " +
-                "\"nameOfCompany\", address, description, phonenumber, email, password, created_at, updated_at, username\n" +
-                "\tFROM public.publisher where username='"+publisherDto.getUsername()+"' and password='"+publisherDto.getPassword()+"'"))
-        {
+        try (
+                Connection connection = DB.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement("SELECT id, " +
+                        "\"nameOfCompany\", address, description, phonenumber, email, password," +
+                        " created_at, updated_at, \"isBlocked\", username\n" +
+                        "\tFROM public.publisher where username='" + publisherDto.getUsername() + "' and password='" + publisherDto.getPassword() + "'")) {
             ResultSet rs = preparedStatement.executeQuery();
             while (rs.next()) {
                 publisher.setId(rs.getLong("id"));
@@ -264,8 +288,10 @@ public class PublisherService {
                 publisher.setEmail(rs.getString("email"));
                 publisher.setPassword(rs.getString("password"));
                 publisher.setDescription(rs.getString("description"));
+                publisher.setBlocked(rs.getBoolean("isBlocked"));
                 publisher.setActive(true);
             }
+            connection.close();
         } catch (SQLException e) {
             // process sql exception
             DB.printSQLException(e);
