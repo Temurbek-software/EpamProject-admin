1becccbf7e9cca2625538e14e6928936de9fdb60
Temurbek-software <temurbek.zaripov140996@mail.ru>
2022-07-27 12:55:30 +0500

    Create ChatController.java

---
+++ src/main/java/controller/ChatController.java
@@ -0,0 +1,33 @@
+package controller;
+
+import services.ChatServices;
+import services.PublisherService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/chat"})
+public class ChatController extends HttpServlet
+{
+
+    private ChatServices chatServices;
+
+    @Override
+    public void init() throws ServletException {
+        chatServices = new ChatServices();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
